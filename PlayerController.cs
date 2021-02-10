b28f07815d3bd2f22dcd7073db76b85589377a90
YosefGuevara012 <yosef.guevara012@hotmail.com>
2021-02-10 01:57:55 -0500

    Adding blend trees

--- PlayerController.cs
+++ PlayerController.cs
@@ -6,7 +6,11 @@ public class PlayerController : MonoBehaviour
 {
 
     public float speed = 5.0f;
-    private const string AXIS_H = "Horizontal", AXIS_V = "Vertical";
+    private bool walking = false;
+    public Vector2 LastMovement = Vector2.zero;
+
+    private const string AXIS_H = "Horizontal", AXIS_V = "Vertical",
+        WALK = "Walking", LAST_H = "LastH", LAST_V = "LastV";
 
     private Animator _animator;
 
@@ -21,11 +25,15 @@ public class PlayerController : MonoBehaviour
     {
         // s = v* t
 
+        walking = false;
+
         if (Mathf.Abs(Input.GetAxisRaw(AXIS_H)) > 0.2f)
         {
             Vector3 translation = new Vector3(
                 Input.GetAxisRaw(AXIS_H) * speed * Time.deltaTime, 0, 0);
             this.transform.Translate(translation);
+            walking = true;
+            LastMovement = new Vector2(Input.GetAxisRaw(AXIS_H), 0);
         }
 
         if (Mathf.Abs(Input.GetAxisRaw(AXIS_V)) > 0.2f)
@@ -33,6 +41,8 @@ public class PlayerController : MonoBehaviour
             Vector3 translation = new Vector3(0,
                 Input.GetAxisRaw(AXIS_V) * speed * Time.deltaTime, 0);
             this.transform.Translate(translation);
+            walking = true;
+            LastMovement = new Vector2(0,Input.GetAxisRaw(AXIS_V));
         }
 
     }
@@ -41,5 +51,8 @@ public class PlayerController : MonoBehaviour
     {
         _animator.SetFloat(AXIS_H, Input.GetAxisRaw(AXIS_H));
         _animator.SetFloat(AXIS_V, Input.GetAxisRaw(AXIS_V));
+        _animator.SetBool(WALK, walking);
+        _animator.SetFloat(LAST_H, LastMovement.x);
+        _animator.SetFloat(LAST_V, LastMovement.y);
     }
 }

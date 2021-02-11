97f942580adafa46e08044ba39c638a8d82f4700
YosefGuevara012 <yosef.guevara012@hotmail.com>
2021-02-11 02:16:16 -0500

    Player collition rules added

--- PlayerController.cs
+++ PlayerController.cs
@@ -13,11 +13,13 @@ public class PlayerController : MonoBehaviour
         WALK = "Walking", LAST_H = "LastH", LAST_V = "LastV";
 
     private Animator _animator;
+    private Rigidbody2D _rigidbody;
 
     // Start is called before the first frame update
     void Start()
     {
         _animator = GetComponent<Animator>();
+        _rigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -29,18 +31,24 @@ public class PlayerController : MonoBehaviour
 
         if (Mathf.Abs(Input.GetAxisRaw(AXIS_H)) > 0.2f)
         {
-            Vector3 translation = new Vector3(
-                Input.GetAxisRaw(AXIS_H) * speed * Time.deltaTime, 0, 0);
-            this.transform.Translate(translation);
+            // Vector3 translation = new Vector3(
+            //    Input.GetAxisRaw(AXIS_H) * speed * Time.deltaTime, 0, 0);
+            // this.transform.Translate(translation);
+
+            _rigidbody.velocity = new Vector2(Input.GetAxisRaw(AXIS_H) * this.speed,
+                                _rigidbody.velocity.y);
             walking = true;
             LastMovement = new Vector2(Input.GetAxisRaw(AXIS_H), 0);
         }
 
         if (Mathf.Abs(Input.GetAxisRaw(AXIS_V)) > 0.2f)
         {
-            Vector3 translation = new Vector3(0,
-                Input.GetAxisRaw(AXIS_V) * speed * Time.deltaTime, 0);
-            this.transform.Translate(translation);
+            // Vector3 translation = new Vector3(0,
+            //    Input.GetAxisRaw(AXIS_V) * speed * Time.deltaTime, 0);
+            // this.transform.Translate(translation);
+
+            _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, 
+                                    Input.GetAxisRaw(AXIS_V) * this.speed);
             walking = true;
             LastMovement = new Vector2(0,Input.GetAxisRaw(AXIS_V));
         }
@@ -49,6 +57,10 @@ public class PlayerController : MonoBehaviour
 
     private void LateUpdate()
     {
+        if (!walking)
+        {
+            _rigidbody.velocity = Vector2.zero;
+        }
         _animator.SetFloat(AXIS_H, Input.GetAxisRaw(AXIS_H));
         _animator.SetFloat(AXIS_V, Input.GetAxisRaw(AXIS_V));
         _animator.SetBool(WALK, walking);
